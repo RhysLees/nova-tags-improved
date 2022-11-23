64d7db92cd3cd9b0a84745ceee2e0b980ba931d6
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2022-11-23 21:51:03 +0000

    Removed old components

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,13 +1,8 @@
-import TagGroup from './components/Tags/TagGroup';
 import TagGroupItem from './components/Tags/TagGroupItem';
-import TagList from './components/Tags/TagList';
 import TagListItem from './components/Tags/TagListItem';
 
 
 Nova.booting((app) => {
-  console.log('BOOTING TAGS')
   app.component("TagGroupItem", TagGroupItem);
   app.component("TagListItem", TagListItem);
-  app.component("TagGroup", TagGroup);
-  app.component("TagList", TagList);
 });
